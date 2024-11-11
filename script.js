99826330a0b5ea2084629be7259fddb9f02918ad
Dexter Sitwell <165820105+Deccatron@users.noreply.github.com>
2024-11-11 22:47:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
+document.getElementById('downloadBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    fetch('https://api.github.com/repos/danlachoi/RezWare/releases/latest')
+        .then(response => response.json())
+        .then(data => {
+            const downloadUrl = data.assets[0].browser_download_url;
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.download = '';
+            link.click();
+        })
+        .catch(error => {
+            console.error('Error fetching the release data:', error);
+        });
+});
